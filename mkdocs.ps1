6f7ad401c99ec1a549ca71420609053ce5911746
AGODA\jlaszlo <jeno.laszlo@agoda.com>
2018-10-11 13:45:25 +0700

    docs fixes

--- mkdocs.ps1
+++ mkdocs.ps1
@@ -9,12 +9,12 @@ $htmls | % {
 
     $html = gc $_.FullName
 
-    $html = $html -replace "../css/", "//agoda-com.github.io/graphql-codegen-csharp/css/"
-    $html = $html -replace "[^/]css/", "//agoda-com.github.io/graphql-codegen-csharp/css/"
-    $html = $html -replace "../js/", "//agoda-com.github.io/graphql-codegen-csharp/js/"
-    $html = $html -replace "[^/]js/", "//agoda-com.github.io/graphql-codegen-csharp/js/"
-    $html = $html -replace "../img/", "//agoda-com.github.io/graphql-codegen-csharp/img/"
-    $html = $html -replace "[^/]img/", "//agoda-com.github.io/graphql-codegen-csharp/img/"
+    $html = $html -replace '../css/', "//agoda-com.github.io/graphql-codegen-csharp/css/"
+    $html = $html -replace '"css/', '"//agoda-com.github.io/graphql-codegen-csharp/css/'
+    $html = $html -replace '../js/', "//agoda-com.github.io/graphql-codegen-csharp/js/"
+    $html = $html -replace '"js/', '"//agoda-com.github.io/graphql-codegen-csharp/js/'
+    $html = $html -replace '../img/', "//agoda-com.github.io/graphql-codegen-csharp/img/"
+    $html = $html -replace '"img/', '"//agoda-com.github.io/graphql-codegen-csharp/img/'
 
     $html | Out-File -FilePath $_.FullName -Encoding utf8
 
