70f43f095efc83f8e5c760b5e6c71e4f95f5a7b1
AGODA\jlaszlo <jeno.laszlo@agoda.com>
2018-10-11 13:33:57 +0700

    added more docs

---
+++ mkdocs.ps1
@@ -0,0 +1,24 @@
+﻿$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+cd $scriptDir
+
+$sitePath = Join-Path $scriptDir "Site"
+
+$htmls = ls $sitePath -Recurse -Filter "*.html" 
+
+$htmls | % {
+
+    $html = gc $_.FullName
+
+    $html = $html -replace "../css/", "//agoda-com.github.io/graphql-codegen-csharp/css/"
+    $html = $html -replace "[^/]css/", "//agoda-com.github.io/graphql-codegen-csharp/css/"
+    $html = $html -replace "../js/", "//agoda-com.github.io/graphql-codegen-csharp/js/"
+    $html = $html -replace "[^/]js/", "//agoda-com.github.io/graphql-codegen-csharp/js/"
+    $html = $html -replace "../img/", "//agoda-com.github.io/graphql-codegen-csharp/img/"
+    $html = $html -replace "[^/]img/", "//agoda-com.github.io/graphql-codegen-csharp/img/"
+
+    $html | Out-File -FilePath $_.FullName -Encoding utf8
+
+    $html
+}
+
+
