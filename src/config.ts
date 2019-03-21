5c06e9e66cfb4a73cf30a0b4a5b18c04aa233bb8
AGODA\jlaszlo <jeno.laszlo@agoda.com>
2019-03-21 10:21:27 +0700

    project structure changes

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-import { GeneratorConfig } from "graphql-codegen-core";
-import { CsharpGeneratorConfig } from "./config/csharpGeneratorConfig";
-export const config: GeneratorConfig = new CsharpGeneratorConfig();
