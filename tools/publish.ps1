8ee80555404cd1c709cb32fb5ed9aa3a2f17c533
AGODA\jlaszlo <jeno.laszlo@agoda.com>
2019-03-25 18:00:31 +0700

    publish changes

--- tools/publish.ps1
+++ tools/publish.ps1
@@ -2,6 +2,15 @@
 
 $scriptDir = Split-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) -Parent
 $nodeProjDir = Join-Path $scriptDir "nodejs"
+$toolsPath = Join-Path $scriptDir "tools"
+
+cd $toolsPath
+
+$nuspecPath = Join-Path $toolsPath "Agoda.CodeGen.GraphQL.nuspec"
+
+$version = $env:APPVEYOR_BUILD_VERSION
+
+nuget pack $nuspecPath -Version "$version-alpha"
 
 cd $nodeProjDir
 
